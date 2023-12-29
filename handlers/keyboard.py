2bf8038d2053aea98fdc8080387bd83a93271ae5
Rintsel <rintsler.druse@gmail.com>
2023-12-29 23:50:50 +0300

    С Юкасса и проработана навигация

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -1,42 +1,32 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
-# Клавиатура с кнопками и настройками размера
+# Клавиатура после оплаты
 menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="💰 Подписка"),
-            KeyboardButton(text="⚙️ HELP"),
-            KeyboardButton(text="📊 Профиль")
+            KeyboardButton(text="📊 Профиль"),
         ],
         [
             KeyboardButton(text="📝 Токены"),
             KeyboardButton(text="👥 Создать чат"),
-            KeyboardButton(text="Назад")
+            KeyboardButton(text="⚙️ HELP")
         ]
     ],
     resize_keyboard=True
 )
 
-# Клавиатура с кнопками и настройками размера
+# Клавиатура до оплаты
 main_menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text="👤 Регистрация"),
-            KeyboardButton(text="👥 Создать чат")
+            KeyboardButton(text="⚙️ HELP"),
+            KeyboardButton(text="👥 Создать чат"),
+            KeyboardButton(text="Запросов осталось")
         ]
     ],
     resize_keyboard=True
 )
-
-# # Новая клавиатура с вариантами подписок
-# subscription_keyboard = ReplyKeyboardMarkup(
-#     keyboard=[
-#         [KeyboardButton(text="Старт")],
-#         [KeyboardButton(text="Комфорт")],
-#         [KeyboardButton(text="Профи")],
-#     ],
-#     resize_keyboard=True
-# )
 
 # InLine Buttons подписки
 inline_markup_submit = InlineKeyboardMarkup(row_width=3)
@@ -48,3 +38,10 @@ pro = InlineKeyboardButton(text="Профи", callback_data="pro")
 inline_markup_submit.insert(start)
 inline_markup_submit.insert(komf)
 inline_markup_submit.insert(pro)
+
+# InLine Buttons регистрации
+inline_markup_reg = InlineKeyboardMarkup(row_width=1)
+
+reg = InlineKeyboardButton(text="👤 Регистрация", callback_data="reg")
+
+inline_markup_reg.insert(reg)
